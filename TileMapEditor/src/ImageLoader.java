a462f046e941319674075887fe160f838d8e36c3
Eirik Strom <eirikstrom1@gmail.com>
2016-08-26 21:50:08 +0200

    Lagt til loadDecorations

--- TileMapEditor/src/ImageLoader.java
+++ TileMapEditor/src/ImageLoader.java
@@ -11,6 +11,8 @@ import sun.applet.Main;
 public class ImageLoader {
 	boolean loaded = false;
 	private HashMap<String,Image> tiles = new HashMap<String,Image>();
+	private HashMap<String,Image> decorations = new HashMap<String,Image>();
+	
 	
 	public void loadFolderTiles(String folderPath){
 		final File folder = new File("src\\tiles");
@@ -39,6 +41,35 @@ public class ImageLoader {
 		}
 		loaded=true;
 	}
+	
+	public void loadDecorations(String folderPath){
+		File folder = new File(folderPath);
+		
+		for (final File fileEntry : folder.listFiles()){
+			if (fileEntry.isDirectory()){
+				loadTiles(fileEntry);
+			} else {
+				String name = "";
+				if (fileEntry.getName().endsWith("jpg")){
+					name=fileEntry.getName();
+					Image image = null;
+					try {
+						image = new Image(fileEntry.toURI().toURL().toString());
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					decorations.put(name,image);
+				}
+			}
+		}
+	}
+	
+	public HashMap<String,Image> getDecorations() {
+		return decorations;
+	}
+	
+	
 	public HashMap<String,Image> getTiles(String folderPath){
 		if (!loaded)
 		loadFolderTiles(folderPath);
